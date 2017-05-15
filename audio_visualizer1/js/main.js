5afbe251840768a1ef7641afaa6d1612159b04ae
Albeforia <cnntcyj@126.com>
2017-05-15 18:58:09 +0800

    audio_visualizer1: make preset a wallpaper option

--- audio_visualizer1/js/main.js
+++ audio_visualizer1/js/main.js
@@ -1,3 +1,20 @@
 window.onload = () => {
-	wallpaperCircle.start();
+	var presets = [wallpaperCube, wallpaperCircle];
+
+	var currIndex = -1;
+
+	window.wallpaperPropertyListener = {
+		applyUserProperties: (properties) => {
+			if (properties.preset) {
+				if (currIndex !== properties.preset.value) {
+					currIndex = properties.preset.value;
+					var old = $('canvas');
+					if (old) {
+						old.remove();
+					}
+					presets[currIndex].start();
+				}
+			}
+		}
+	}
 };
